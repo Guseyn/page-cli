153c3bdeaead7fbd2a4843735628393c88ac22e5
gismayilov <guseynism@gmail.com>
2018-11-14 17:42:03 +0300

    ask questions of details of project

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const { as } = require('@cuties/cutie');
+const { CreatedInterface, AnswersOfQuestionedInterface, ClosedInterface } = require('@cuties/readline');
+const { ExitedProcess } = require('@cuties/process');
+const CreatedPageSkeleton = require('./CreatedPageSkeleton');
+
+if (process.argv[2] === 'create') {
+  new CreatedInterface({
+    input: process.stdin,
+    output: process.stdout
+  }).as('interface').after(
+    new CreatedPageSkeleton(
+      new AnswersOfQuestionedInterface(
+        as('interface'), 'License: (MIT) ',
+        new AnswersOfQuestionedInterface(
+          as('interface'), 'Description: ',
+          new AnswersOfQuestionedInterface(
+            as('interface'), 'Author: ',
+            new AnswersOfQuestionedInterface(
+              as('interface'), 'Version: (1.0.0) ',
+              new AnswersOfQuestionedInterface(
+                as('interface'), 'Project name: '
+              )
+            )
+          )
+        ) 
+      )
+    ).after(
+      new ClosedInterface(as('interface')).after(
+        new ExitedProcess(process, 0)
+      )
+    )
+  ).call();
+}
